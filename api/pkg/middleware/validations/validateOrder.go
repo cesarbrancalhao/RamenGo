f62a73f9f30ca654654e23391b8abb4035e15ec9
Brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-05-31 01:16:10 -0300

    Updated validations

--- api/pkg/middleware/validations/validateOrder.go
+++ api/pkg/middleware/validations/validateOrder.go
@@ -3,18 +3,20 @@ package middleware
 import (
 	"api/internal/models"
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
 
-func ValidateOrder() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var orderRequest models.OrderRequest
-		if err := c.ShouldBindJSON(&orderRequest); err != nil {
-			c.JSON(http.StatusBadRequest, models.ErrorResponse{Error: err.Error()})
-			return
-		}
+func ValidateOrderRequest(c *gin.Context, order *models.OrderRequest) {
 
-		c.Next()
+	brothIdRegex := regexp.MustCompile(`^[1-3]+$`)
+	proteinIdRegex := regexp.MustCompile(`^[1-3]+$`)
+
+	if !brothIdRegex.MatchString(order.BrothId) {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{Error: "Invalid brothId"})
+	}
+	if !proteinIdRegex.MatchString(order.ProteinId) {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.ErrorResponse{Error: "Invalid proteinId"})
 	}
 }
