c9032edf4de4be794de18b8a9e1d8c17d02fb0bd
Brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-06-02 20:13:55 -0300

    Fixed cards images

--- client/src/scripts/view/cards.js
+++ client/src/scripts/view/cards.js
@@ -1,4 +1,5 @@
 import { getBroths, getProteins } from "../controller/apiController";
+import { turnBtnActive } from "./order";
 
 const brothCardContainer = document.querySelector('#broth');
 const proteinCardContainer = document.querySelector('#protein');
@@ -7,30 +8,64 @@ const btnCenter = document.querySelector('.btn-center');
 
 const fadeIn = (element) => element.animate([{opacity: 0}, {opacity: 1}], {duration: 900, fill: 'forwards'});
 
-
-const selectCard = (card, container) => {
-    if (card.classList.contains('selected')) card.classList.remove('selected');
-    else {
-        Array.from(container.children).forEach(child => child.classList.remove('selected'));
-        card.classList.add('selected');
-    }
-};
-
 const createList = (list, container) => {
     for (const x of Object.entries(list)) {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
-            <img src="${x[1].imageInactive}">
+            <img class="card-image" id="inactive" src="${x[1].imageInactive}">
+            <img class="card-image hidden" id="active" src="${x[1].imageActive}">
             <p class="card-title">${x[1].name}</p>
             <p class="card-description">${x[1].description}</p>
             <p class="card-price">US$ ${x[1].price}</p>`;
-        card.onclick = () => selectCard(card, container);
+        card.onclick = (e) => { 
+            e.stopPropagation(); 
+            selectCard(card, container); };
+        card.addEventListener('click', () => turnBtnActive(document.querySelector('#order'), 
+            () => {}));
         container.appendChild(card);
 
         fadeIn(card);
     }
-}
+};
+
+const removeActive = card => {
+    card.classList.remove('selected');
+
+    const imgActive = card.querySelector('#active');
+    const imgInactive = card.querySelector('#inactive');
+
+    imgActive.classList.add('hidden');
+    imgInactive.classList.remove('hidden');
+};
+
+const turnActive = (card, container) => {
+    Array.from(container.children).forEach(child => {
+        if (child !== card) {
+
+            child.classList.remove('selected');
+
+            const imgActive = child.querySelector('#active');
+            const imgInactive = child.querySelector('#inactive');
+
+            imgActive.classList.add('hidden');
+            imgInactive.classList.remove('hidden');
+        }
+    });
+
+    card.classList.add('selected');
+    const imgActive = card.querySelector('#active');
+    const imgInactive = card.querySelector('#inactive');
+
+    imgActive.classList.remove('hidden');
+    imgInactive.classList.add('hidden');
+};
+
+const selectCard = (card, container) => {
+    card.classList.contains('selected') ?
+        removeActive(card) : 
+        turnActive(card, container);
+};
 
 export async function fillCards() {
     const brothList = await getBroths();
@@ -41,4 +76,4 @@ export async function fillCards() {
     createList(proteinList, proteinCardContainer);
     fadeIn(mainText[1]);
     fadeIn(btnCenter);
-}
+};
