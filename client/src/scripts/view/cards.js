97aa4ea6b923c7a01db72e7586efca15358b73c3
Brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-06-02 16:54:34 -0300

    Added animations for page load

--- client/src/scripts/view/cards.js
+++ client/src/scripts/view/cards.js
@@ -2,6 +2,11 @@ import { getBroths, getProteins } from "../controller/apiController";
 
 const brothCardContainer = document.querySelector('#broth');
 const proteinCardContainer = document.querySelector('#protein');
+const firstMainText = document.querySelector('#main-t1');
+const secondMainText = document.querySelector('#main-t2');
+const btnCenter = document.querySelector('.btn-center');
+
+const fadeIn = (element) => element.animate([{opacity: 0}, {opacity: 1}], {duration: 900, fill: 'forwards'});
 
 const createList = (list, container) => {
     for (const x of Object.entries(list)) {
@@ -14,6 +19,8 @@ const createList = (list, container) => {
             <p class="card-price">US$ ${x[1].price}</p>
         `;
         container.appendChild(card);
+
+        fadeIn(card);
     }
 }
 
@@ -21,6 +28,9 @@ export async function fillCards() {
     const brothList = await getBroths();
     const proteinList = await getProteins();
 
+    fadeIn(firstMainText);
     createList(brothList, brothCardContainer);
     createList(proteinList, proteinCardContainer);
+    fadeIn(secondMainText);
+    fadeIn(btnCenter);
 }
