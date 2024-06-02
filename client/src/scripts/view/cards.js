4108371aaafb1f427c7e874ea42500805f409237
Brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-06-02 17:56:29 -0300

    Added select card function

--- client/src/scripts/view/cards.js
+++ client/src/scripts/view/cards.js
@@ -2,11 +2,19 @@ import { getBroths, getProteins } from "../controller/apiController";
 
 const brothCardContainer = document.querySelector('#broth');
 const proteinCardContainer = document.querySelector('#protein');
-const firstMainText = document.querySelector('#main-t1');
-const secondMainText = document.querySelector('#main-t2');
+const mainText = document.querySelectorAll('.main-text');
 const btnCenter = document.querySelector('.btn-center');
 
 const fadeIn = (element) => element.animate([{opacity: 0}, {opacity: 1}], {duration: 900, fill: 'forwards'});
+
+
+const selectCard = (card, container) => {
+    if (card.classList.contains('selected')) card.classList.remove('selected');
+    else {
+        Array.from(container.children).forEach(child => child.classList.remove('selected'));
+        card.classList.add('selected');
+    }
+};
 
 const createList = (list, container) => {
     for (const x of Object.entries(list)) {
@@ -16,8 +24,8 @@ const createList = (list, container) => {
             <img src="${x[1].imageInactive}">
             <p class="card-title">${x[1].name}</p>
             <p class="card-description">${x[1].description}</p>
-            <p class="card-price">US$ ${x[1].price}</p>
-        `;
+            <p class="card-price">US$ ${x[1].price}</p>`;
+        card.onclick = () => selectCard(card, container);
         container.appendChild(card);
 
         fadeIn(card);
@@ -28,9 +36,9 @@ export async function fillCards() {
     const brothList = await getBroths();
     const proteinList = await getProteins();
 
-    fadeIn(firstMainText);
+    fadeIn(mainText[0]);
     createList(brothList, brothCardContainer);
     createList(proteinList, proteinCardContainer);
-    fadeIn(secondMainText);
+    fadeIn(mainText[1]);
     fadeIn(btnCenter);
 }
