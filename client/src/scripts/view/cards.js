1763ce77e31d2c22a7cc1c9e95cf85976f367d04
Brancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-06-02 15:22:32 -0300

    Created card filler

--- client/src/scripts/view/cards.js
+++ client/src/scripts/view/cards.js
@@ -1,4 +1,26 @@
 import { getBroths, getProteins } from "../controller/apiController";
 
-getBroths()
-getProteins()
+const brothCardContainer = document.querySelector('#broth');
+const proteinCardContainer = document.querySelector('#protein');
+
+const createList = (list, container) => {
+    for (const x of Object.entries(list)) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${x[1].imageInactive}">
+            <p class="card-title">${x[1].name}</p>
+            <p class="card-description">${x[1].description}</p>
+            <p class="card-price">US$ ${x[1].price}</p>
+        `;
+        container.appendChild(card);
+    }
+}
+
+export async function fillCards() {
+    const brothList = await getBroths();
+    const proteinList = await getProteins();
+
+    createList(brothList, brothCardContainer);
+    createList(proteinList, proteinCardContainer);
+}
