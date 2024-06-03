29d8f098f95b4c5ed096079a8d95269df5d7068a
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-06-03 14:42:07 -0300

    Updated placeholders urls

--- client/src/scripts/config/placeholders.js
+++ client/src/scripts/config/placeholders.js
@@ -3,24 +3,24 @@
 const proteins = [
     {
         "id": 1,
-        "imageInactive": "https://tech.redventures.com.br/icons/pork/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/pork/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-pork.svg",
+        "imageActive": "./src/assets/icons/active/active-pork.svg",
         "name": "Chasu",
         "description": "A sliced flavourful pork meat with a selection of season vegetables.",
         "price": 10
     },
     {
         "id": 2,
-        "imageInactive": "https://tech.redventures.com.br/icons/yasai/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/yasai/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-yasai.svg",
+        "imageActive": "./src/assets/icons/active/active-yasai.svg",
         "name": "Yasai Vegetarian",
         "description": "A delicious vegetarian lamen with a selection of season vegetables.",
         "price": 10
     },
     {
         "id": 3,
-        "imageInactive": "https://tech.redventures.com.br/icons/chicken/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/chicken/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-chicken.svg",
+        "imageActive": "./src/assets/icons/active/active-chicken.svg",
         "name": "Karaague",
         "description": "Three units of fried chicken, moyashi, ajitama egg and other vegetables.",
         "price": 0
@@ -30,24 +30,24 @@ const proteins = [
 const broths = [
     {
         "id": 1,
-        "imageInactive": "https://tech.redventures.com.br/icons/salt/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/salt/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-salt.svg",
+        "imageActive": "./src/assets/icons/active/active-salt.svg",
         "name": "Salt",
         "description": "Simple like the seawater, nothing more",
         "price": 10
     },
     {
         "id": 2,
-        "imageInactive": "https://tech.redventures.com.br/icons/miso/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/miso/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-miso.svg",
+        "imageActive": "./src/assets/icons/active/active-miso.svg",
         "name": "Miso",
         "description": "Paste made of fermented soybeans",
         "price": 12
     },
     {
         "id": 3,
-        "imageInactive": "https://tech.redventures.com.br/icons/shoyu/inactive.svg",
-        "imageActive": "https://tech.redventures.com.br/icons/shoyu/active.svg",
+        "imageInactive": "./src/assets/icons/inactive/inactive-shoyu.svg",
+        "imageActive": "./src/assets/icons/active/active-shoyu.svg",
         "name": "Shoyu",
         "description": "The good old and traditional soy sauce",
         "price": 10
@@ -58,76 +58,70 @@ const recipes = [
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Salt and Chasu Ramen",
-        "image": "../../assets/dishes/chasu.png"
+        "image": "./src/assets/dishes/chasu.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Salt and Yasai Vegetarian Ramen",
-        "image": "../../assets/dishes/yasai.png"
+        "image": "./src/assets/dishes/yasai.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Salt and Karaague Ramen",
-        "image": "../../assets/dishes/karaague.png"
+        "image": "./src/assets/dishes/karaague.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Shoyu and Chasu Ramen",
-        "image": "../../assets/dishes/chasu.png"
+        "image": "./src/assets/dishes/chasu.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Shoyu and Yasai Vegetarian Ramen",
-        "image": "../../assets/dishes/yasai.png"
+        "image": "./src/assets/dishes/yasai.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Shoyu and Karaague Ramen",
-        "image": "../../assets/dishes/karaague.png"
+        "image": "./src/assets/dishes/karaague.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Miso and Chasu Ramen",
-        "image": "../../assets/dishes/chasu.png"
+        "image": "./src/assets/dishes/chasu.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Miso and Yasai Vegetarian Ramen",
-        "image": "../../assets/dishes/yasai.png"
+        "image": "./src/assets/dishes/yasai.png"
     },
     {
         "id": Math.floor(Math.random() * 100000),
         "description": "Miso and Karaague Ramen",
-        "image": "../../assets/dishes/karaague.png"
+        "image": "./src/assets/dishes/karaague.png"
     },
     {
         "id": Math.floor(Math.random() * 100000)
     }
 ];
-const placeholders = {
-    proteins: { items: proteins },
-    broths: { items: broths },
-    recipes: { items: recipes }
-};
-
 const findOrderRecipeId = (b, p) => {
     if (b === "0" || p === "0") return recipes[9];
     const brothMap = {"1": 0, "2": 3, "3": 6};
     const proteinMap = {"1": 1, "2": 2, "3": 3};
     
     let responseId = String(parseInt(brothMap[b]) + proteinMap[p]);
-
+    
     return recipes[parseInt(responseId) - 1];
 };
 
 export const getPlaceHolderByName = (name, b = 0, p = 0) => {
     switch (name) {
         case "proteins":
-            return placeholders.proteins.items;
+            return proteins;
         case "broths":
-            return placeholders.broths.items;
+            return broths;
         case "recipes":
-            return placeholders.recipes.items;
+            return recipes;
         case "orders":
             return findOrderRecipeId(b, p);
     }
